5a3122f887966fcbefed894d08f5bbe91c1c0766
Andrey <n1an27f@gmail.com>
2022-07-07 00:04:02 +1000

    1st add

---
+++ hwg_ll/hwg_ll/APIHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+
+namespace hwg_ll
+{
+    public class APIHelper
+    {
+        HttpClient Client = new HttpClient();
+
+        public async Task<string> Get_response()
+        {
+            string appid = "2f5c4fab286fa279ae7141338e7e967a";
+            List<Coord_Response> coords = JsonConvert.DeserializeObject<List<Coord_Response>>(await Get_coord());
+            double lat = coords[0].lat;
+            double lon = coords[0].lon;
+            Console.WriteLine(lat + "  " + lon);
+
+            string curr_weather_rq = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat.ToString() + "&lon=" + lon.ToString() + "&appid=" + appid;
+
+            string responseTask = await Client.GetStringAsync(curr_weather_rq);
+
+            return responseTask;
+        }
+
+        public async Task<string> Get_coord(string city = "Moscow")
+        {
+            string appid = "2f5c4fab286fa279ae7141338e7e967a";
+
+            string coord_rq = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + appid;
+            string get_coord = await Client.GetStringAsync(coord_rq);
+            //Coord_Response coords = JsonConvert.DeserializeObject<Coord_Response>(get_coord);
+            return get_coord;
+        }
+
+        public class APIResponse
+        {
+            public bool Success => ErrorMessage == null;
+            public string ErrorMessage { get; set; }
+            public string Response { get; set; }
+        }
+    }
+}
---
+++ hwg_ll/hwg_ll/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace hwg_ll
+{
+    public partial class MainPage : ContentPage
+    {
+        bool anim = false;
+        float speed;
+
+        public async void Anim_propeller(object sender, System.EventArgs e)
+        {
+            anim = !anim;
+
+            if (!anim) { anim_btn.Text = "Enable animation"; return; }
+            else if (anim) { anim_btn.Text = "Disable animation"; }
+
+            int final_speed = (int)speed * 1000;
+            while (anim)
+            {
+                await propeller.RelRotateTo(360, (uint)final_speed);
+            }
+        }
+
+        public string ConvertTime(double unixTimeStamp)
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            string dt = dtDateTime.ToString();
+            string[] dtL = dt.Split(' ');
+
+            return dtL[0];
+        }
+
+        public string FindDirection(int deg)
+        {
+            double val = Math.Floor((deg / 22.5) + 0.5);
+            string[] arr = new string[] { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" };
+            var dir = val % 16;
+            return arr[(int)dir];
+        }
+
+        public async void Share_info(object sender, System.EventArgs e)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Title = $"Погода в городе {city.Text}",
+                Text = $"На данный момент температура в городе {city.Text} равна: {temp.Text}. \n Влажность: {humidity.Text}. \n Скорость ветра: {wind.Text}. \n Направление ветра: {wind_dir.Text}. \n Состояние погоды: {weather.Text}."
+            });
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            APIHelper aPIHelper = new APIHelper();
+
+            GetResponse();
+
+            async void GetResponse()
+            {
+                string result = await aPIHelper.Get_response();
+                JObject json = JObject.Parse(result);
+
+                string img_icon = "https://openweathermap.org/img/wn/" + json["weather"][0]["icon"].ToString() + "@4x.png";
+                int wind_deg = int.Parse(json["wind"]["deg"].ToString());
+                string wind_speed = json["wind"]["speed"].ToString();
+
+                speed = float.Parse(wind_speed);
+                city.Text = json["name"].ToString();
+                float tempCel = float.Parse(json["main"]["temp"].ToString());
+                double dt = double.Parse(json["dt"].ToString());
+                temp.Text = Math.Round(tempCel - 273.15, 1).ToString();
+                weather.Text = json["weather"][0]["main"].ToString();
+                img_weather.Source = new UriImageSource
+                {
+                    CachingEnabled = false,
+                    Uri = new System.Uri(img_icon)
+                };
+                datetime.Text = ConvertTime(dt).ToString();
+                humidity.Text = json["main"]["humidity"].ToString() + "%";
+                wind.Text = json["wind"]["speed"].ToString() + " m/s";
+                wind_dir.Text = FindDirection(wind_deg);
+                arrow.Rotation = wind_deg;
+
+                newlay.ResolveLayoutChanges();
+            }
+        }
+    }
+}
